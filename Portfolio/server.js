12632603802f1f8ae6fb2b3d8e4f68bf751e2894
Vinay <vinayvivek070@gmail.com>
2025-09-11 18:52:23 +0530

    Update server.js

--- Portfolio/server.js
+++ Portfolio/server.js
@@ -7,7 +7,7 @@ const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 // Middleware
 app.use(helmet());
