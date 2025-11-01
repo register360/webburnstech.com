7cc8977643caf5263fcb310a045d32d7a8e9f096
Vinay <vinayvivek070@gmail.com>
2025-11-01 14:48:43 +0530

    Update server.js

--- Webburns Manager/server.js
+++ Webburns Manager/server.js
@@ -42,9 +42,12 @@ const storage = getStorage(firebaseApp);
 // Resend Configuration
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const MistralClient = require("@mistralai/mistralai").default;
+const { MistralClient } = require("@mistralai/mistralai");
 // Initialize Mistral client
-const mistral = new MistralClient(process.env.MISTRAL_API_KEY);
+const mistral = MistralClient({
+  apiKey: process.env.MISTRAL_API_KEY
+});
+
 
 // Middleware
 app.use(cors());
