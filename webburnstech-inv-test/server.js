6458d6523e1ce3adf7f2598338b9cc3247c55804
Vinay <vinayvivek070@gmail.com>
2025-11-28 20:57:46 +0530

    Update server.js

--- webburnstech-inv-test/server.js
+++ webburnstech-inv-test/server.js
@@ -135,10 +135,6 @@ async function sendExamCredentials() {
   }
 }
 
-// Email service using Resend
-const { Resend } = require('resend');
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 async function sendEmail({ to, subject, html, text }) {
   try {
     const { data, error } = await resend.emails.send({
